9539afc64dfd6d6a1da516aa62618723369ace34
Nats-ji <angel_tommy@foxmail.com>
2021-02-10 06:07:35 +0800

    Update copyright notice

--- init.lua
+++ init.lua
@@ -1,19 +1,20 @@
--- Copyright 2021 Mingming Cui
+-- Cyberpunk Advanced Settings Tool allows you to tweak Cyberpunk 2077's
+-- engine settings in-game.
+
+-- Copyright (C) 2021 Mingming Cui
 --
--- This file is part of Cyberpunk Advanced Settings Tool.
---
--- Cyberpunk Advanced Settings Tool is free software: you can redistribute it and/or modify
+-- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 --
--- Cyberpunk Advanced Settings Tool is distributed in the hope that it will be useful,
+-- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 registerForEvent("onInit", function()
   CPS = require("CPStyling")
